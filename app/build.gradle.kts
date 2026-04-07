4b523e28b6dfdbfffdf53ddd1c5686b267aabffa
Fabi <git@fthomys.me>
2026-04-07 14:00:58 +0200

    Bump version to 1.1.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "me.diamondforge.tokn"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
